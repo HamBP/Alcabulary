64594126c258116630b320ed2b24b6e2f6baadf4
algosketch <sjy_9484@naver.com>
2021-04-15 06:15:55 +0900

    add : 스플래시 화면, 예문 공부 화면 레이아웃 add : 메인 화면과 추가, 예문 화면 연결

--- app/src/main/java/org/algosketch/alcabulary/MainActivity.kt
+++ app/src/main/java/org/algosketch/alcabulary/MainActivity.kt
@@ -1,15 +1,22 @@
 package org.algosketch.alcabulary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        findViewById<Button>(R.id.main_btn_add).setOnClickListener {
+            val intent = Intent(this, AddVocabularyActivity::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.main_btn_study).setOnClickListener {
+            val intent = Intent(this, StudyExampleActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
 }
