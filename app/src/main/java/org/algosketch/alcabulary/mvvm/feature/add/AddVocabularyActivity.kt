958690937ade1973f4660a2d920412a6763ecd7f
algosketch <sjy_9484@naver.com>
2021-04-15 07:01:32 +0900

    room database 더미 데이터 넣기까지 성공

--- app/src/main/java/org/algosketch/alcabulary/mvvm/feature/add/AddVocabularyActivity.kt
+++ app/src/main/java/org/algosketch/alcabulary/mvvm/feature/add/AddVocabularyActivity.kt
@@ -2,11 +2,30 @@ package org.algosketch.alcabulary.mvvm.feature.add
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.room.Room
 import org.algosketch.alcabulary.R
+import org.algosketch.alcabulary.mvvm.model.db.AppDatabase
+import org.algosketch.alcabulary.mvvm.model.db.Vocabulary
 
 class AddVocabularyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_vocabulary)
+
+        findViewById<Button>(R.id.add_btn_and_main).setOnClickListener {
+            val db = Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java,
+                "vocabulary"
+            ).build()
+            Thread {
+                val dao = db.VocabularyDao()
+                dao.insertVocabulary(Vocabulary(1, "resume", "이력서", "I submitted resume."))
+                Log.d("데이터 삽입 : ", "결과 : " + dao.getAll()[0].exampleSentence)
+            }.start()
+            finish()
+        }
     }
 }
--- app/src/main/java/org/algosketch/alcabulary/mvvm/model/db/Vocabulary.kt
+++ app/src/main/java/org/algosketch/alcabulary/mvvm/model/db/Vocabulary.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Vocabulary (
-    @PrimaryKey val vid: Int,
-    @ColumnInfo(name = "vocabulary") val vocabulary: String,
-    @ColumnInfo(name = "meaning") val meaning: String,
+    @PrimaryKey(autoGenerate = true) val vid: Int,
+    val vocabulary: String,
+    val meaning: String,
     @ColumnInfo(name = "example_sentence") val exampleSentence: String?
     )
