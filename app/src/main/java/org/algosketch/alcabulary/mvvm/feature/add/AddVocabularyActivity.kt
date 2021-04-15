1e61c35ad6b3ad5f5c82e04672c9b9c50f445980
algosketch <sjy_9484@naver.com>
2021-04-16 07:48:30 +0900

    add : 단어 추가 후 추가 기능 add : 단어 추가 빈칸 제출시 예외처리

--- app/src/main/java/org/algosketch/alcabulary/mvvm/feature/add/AddVocabularyActivity.kt
+++ app/src/main/java/org/algosketch/alcabulary/mvvm/feature/add/AddVocabularyActivity.kt
@@ -2,24 +2,58 @@ package org.algosketch.alcabulary.mvvm.feature.add
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
 import org.algosketch.alcabulary.R
 
 class AddVocabularyActivity : AppCompatActivity() {
     private val viewModel: AddVocabularyViewModel by viewModels()
 
+    private var inputWord: EditText? = null
+    private var inputMeaning: EditText? = null
+    private var inputSentence: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_vocabulary)
 
+        inputWord = findViewById(R.id.add_edit_vocabulary)
+        inputMeaning = findViewById(R.id.add_edit_meaning)
+        inputSentence = findViewById(R.id.add_edit_example_sentence)
+
+        // 완료 후 메인 버튼
         findViewById<Button>(R.id.add_btn_and_main).setOnClickListener {
-            val word = findViewById<EditText>(R.id.add_edit_vocabulary).text.toString()
-            val meaning = findViewById<EditText>(R.id.add_edit_meaning).text.toString()
-            val sentence = findViewById<EditText>(R.id.add_edit_example_sentence).text.toString()
-            viewModel.postWord(word, meaning, sentence)
-            finish()
+            if(isCorrect()) {
+                request()
+                finish()
+            }
+            else Toast.makeText(this, "단어 혹은 뜻을 입력하지 않았습니다.", Toast.LENGTH_SHORT).show()
         }
+
+        // 완료 후 하나 더 추가 버튼
+        findViewById<Button>(R.id.add_btn_and_add).setOnClickListener {
+            if(isCorrect()) {
+                request()
+                finish()
+            }
+            else Toast.makeText(this, "단어 혹은 뜻을 입력하지 않았습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // 코드 중복 무엇...?
+    private fun isCorrect(): Boolean {
+        val word = inputWord?.text.toString()
+        val meaning = inputMeaning?.text.toString()
+        return word.isNotEmpty() && meaning.isNotEmpty()
+    }
+
+    private fun request() {
+        val word = inputWord?.text.toString()
+        val meaning = inputMeaning?.text.toString()
+        val sentence = inputSentence?.text.toString()
+        viewModel.postWord(word, meaning, sentence)
     }
 }
