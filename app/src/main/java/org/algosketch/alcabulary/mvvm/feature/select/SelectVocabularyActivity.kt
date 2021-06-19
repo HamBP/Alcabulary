1c4d90c92970e9c463bdf86a198a2b81f8464442
algosketch <sjy_9484@naver.com>
2021-06-19 19:40:45 +0900

    add SelectVocabulary viewModel

--- app/src/main/java/org/algosketch/alcabulary/mvvm/feature/select/SelectVocabularyActivity.kt
+++ app/src/main/java/org/algosketch/alcabulary/mvvm/feature/select/SelectVocabularyActivity.kt
@@ -2,11 +2,19 @@ package org.algosketch.alcabulary.mvvm.feature.select
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import org.algosketch.alcabulary.R
+import org.algosketch.alcabulary.databinding.ActivitySelectVocabularyBinding
 
 class SelectVocabularyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySelectVocabularyBinding
+    private val viewModel: SelectVocabularyViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_vocabulary)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_vocabulary)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
