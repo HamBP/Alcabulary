05e6eb80fc3752c56fad271e638e88c7520d87af
algosketch <sjy_9484@naver.com>
2021-06-28 20:30:22 +0900

    load data from selectVocabulary

--- app/src/main/java/org/algosketch/alcabulary/mvvm/feature/select/SelectVocabularyViewModel.kt
+++ app/src/main/java/org/algosketch/alcabulary/mvvm/feature/select/SelectVocabularyViewModel.kt
@@ -1,7 +1,22 @@
 package org.algosketch.alcabulary.mvvm.feature.select
 
 import androidx.lifecycle.ViewModel
+import androidx.room.Room
+import org.algosketch.alcabulary.mvvm.global.MyApplication
+import org.algosketch.alcabulary.mvvm.model.db.AppDatabase
+import org.algosketch.alcabulary.mvvm.model.db.VocabularyDao
 
 class SelectVocabularyViewModel : ViewModel() {
+    init {
+        val db = Room.databaseBuilder(
+            MyApplication.ApplicationContext(),
+            AppDatabase::class.java,
+            "vocabulary"
+        ).build()
 
+        Thread {
+            val words = db.VocabularyDao().getAll()
+            // TODO : 아답터에 데이터 삽입
+        }
+    }
 }
